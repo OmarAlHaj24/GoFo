98c2349696dadf7eec242750de5da078c2563f91
miretteamin <miretteamin@gmail.com>
2021-06-11 13:59:46 +0200

    Modifying Main

--- SW/src/Main.java
+++ SW/src/Main.java
@@ -1,14 +1,14 @@
+import System.SystemGoFo;
+
 /**
  * Main Class
  *
  * @author Omar Khaled Al Haj      20190351
  * @author Mirette Amin Danial     20190570
  * @author Mostafa Mahmoud Anwar   20190544
- * Created on 6/6/2021
  * @version 1.0
+ * @since 4 June 2021
  */
-
-import System.SystemGoFo;
 
 public class Main {
 
